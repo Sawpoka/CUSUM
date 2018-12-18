405a6f92d94971236ad1d958dfda60eebcbcfbf0
Sawpoka <soeren.sander@gmx.de>
2018-12-18 13:44:54 +0100

    new version

--- test_cusum.py
+++ test_cusum.py
@@ -1,20 +1,33 @@
-# Testplot
-
-# laden der benötigten Bibliotheken
+# laden der benötigten Bibliotheken:
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Einlesen des Datensatzes. Erste Spalte in time und zweite Spalte in event_value
 time, event_value = np.loadtxt("Aufgabe7_CUSUM.txt", unpack = True)
 
-#Einlesen eines geschätzten Mittelwertes.
-w = float(input("Mittelwert eingeben: "))
+# Einlesen/Ausrechnen eines Mittelwertes:
+# w = float(input("Mittelwert eingeben: "))
+w = np.average(event_value)
+std_dev = np.std(event_value)
+print(std_dev)
+print(w)
+
+# Durchführen des Cusums verfahrens: Berechnung der Abweichungen und ausgeben des Summer dieser
+
+c = 0.0
+i = 0
+while i < len(event_value):
+    c = c + (event_value[i] - w)
+    i += 1
+
+
+print(c)
 
 
 
+# Plotten der Ereignissanzahl (y-Achse) auf die Zeit (x-Achse):
+# TODO: Mit Marker und Zeitstempel
 
-
-# Plotten der Ereignissanzahl (y-Achse) auf die Zeit (x-Achse)
 plt.plot(time, event_value, "g-")
 plt.xlabel('Time')
 plt.ylabel('Events')
