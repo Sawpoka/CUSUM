8188db6a069123fcc431cd88b182a5363b85334f
Sawpoka <soeren.sander@gmx.de>
2019-01-15 13:51:20 +0100

    aktuelle Version

--- cusum_neu.py
+++ cusum_neu.py
@@ -1,39 +1,52 @@
+# CUSUM New - Formel nach Wikipedia - Orientierung an cusum_felix und test_cusum
+
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 import time as tm
+from datetime import datetime
+from datetime import timedelta
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-
+#Startdatum 20.07.1999
+startdate = '20.07.1999'
+#Umwandlung in ein passendes Format
+startdate_c = datetime.strptime(startdate,'%d.%m.%Y')
 
 time, event_value = np.loadtxt("Aufgabe7_CUSUM.txt", unpack = True)
 
-# print("Der eingelesene Datensatz enthält " + str(len(time)) + " Werte.")
-#
+"""print("Der eingelesene Datensatz enthaelt " + str(len(time)) + " Werte.")
+
 # Eingabe der Teilanzahl des Datensatzes die mit dem Programm getestet werden sollen
-# number = int(input("Anzahl der zu testenen Daten eingeben: ")) # = len(event_value)
-# if number > len(time):
-#    print("Number ist größer als der Datensatz (" + str(len(time)) + " Werte)")
-#    exit()          # Beendet das Programm, falls _number_ zu groß ist
-#
-
+number = int(input("Anzahl der zu testenen Daten eingeben: ")) # = len(event_value)
+if number > len(time):
+    print("Number ist groesser als der Datensatz (" + str(len(time)) + " Werte)")
+    exit()          # Beendet das Programm, falls _number_ zu gross ist
+"""
 start = tm.time()
+number = len(time)
+w = np.mean(event_value)
+print("Die Messung wurde am ",startdate_c+timedelta(minutes=time[0]),"gestartet und am ",startdate_c+timedelta(minutes=time[0]),"beendet" )                                       # Erwartungswert
+print("Der Mittelwert der Messwerte betraegt: " + str(w))
 
-number = (len(time))
 
-w = np.mean(event_value)                                        # Erwartungswert
-print("Der Mittelwert der Messwerte beträgt: " + str(w))
-
-
-s = [.0] * number
-sp = [.0] * number
-sn = [.0] * number
-detected_points = [0] * number
+s = [0] * number
+sp = [0] * number
+sn = [0] * number
+detected_points = []
+converted_time = [0] * number #Array for saving of the converted times
 time_s = time[:number]
-h = 5*w                   # Grenzwert
+h = 100*w                   # Grenzwert
 i = 0
-bool = True
-boolp = True
-booln = True
+
+#converting the time array into dates
+j = 0
+while j < len(time):
+    converted_time[j] = startdate_c + timedelta(minutes=time[j])
+    j+=1
+
 while i < number:
     #ALT: value_negative = min(0, event_value[i] - w)
     #ALT: value_positive = max(0, event_value[i] - w)
@@ -41,55 +54,76 @@ while i < number:
     if i == 0:
         s[i] = value_actual
         sp[i] = value_actual
-        if sp[i] < 0: sp[i] = 0  # sp wird nie < 0
+        if sp[i] < 0:
+            sp[i] = 0  # sp wird nie < 0
         sn[i] = value_actual
-        if sn[i] > 0: sp[i] = 0  # sn wird nie > 0
+        if sn[i] > 0:
+            sp[i] = 0  # sn wird nie > 0
     else:
         s[i] = s[i-1] + value_actual    #ALT: + (event_value[i] - w)
         sp[i] = sp[i-1] + value_actual
-        if sp[i] < 0: sp[i] = 0     # sp wird nie < 0
+        if sp[i] < 0:
+            sp[i] = 0     # sp wird nie < 0
         sn[i] = sn[i-1] + value_actual
-        if sn[i] > 0: sn[i] = 0     # sn wird nie > 0
+        if sn[i] > 0:
+            sn[i] = 0     # sn wird nie > 0
 
-    if (s[i] < -h or s[i] > h) and bool:
+    if (s[i] < -h or s[i] > h):
         print("CUSUM an der Stelle  " + str(i) + "  zur Zeit  " + str(time[i]) + "  Minuten ab Start der Sonde.")
-        bool = False
-    if sp[i] > h and boolp:
+        s[i] = value_actual
+        sp[i] = value_actual
+        detected_points.append(startdate_c + timedelta(minutes=time[i]))
+        if sp[i] < 0:
+            sp[i] = 0  # sp wird nie < 0
+        sn[i] = value_actual
+        if sn[i] > 0:
+            sp[i] = 0  # sn wird nie > 0
+        i = i + 1000
+    if sp[i] > h:
         print("CUSUM+ an der Stelle  " + str(i) + "  zur Zeit  " + str(time[i]) + "  Minuten ab Start der Sonde.")
-        boolp = False
-    if sn[i] < -h and booln:
+        s[i] = value_actual
+        sp[i] = value_actual
+        if sp[i] < 0:
+            sp[i] = 0  # sp wird nie < 0
+        sn[i] = value_actual
+        if sn[i] > 0:
+            sp[i] = 0  # sn wird nie > 0
+        detected_points.append(startdate_c + timedelta(minutes=time[i]))
+        i += 1000
+    if sn[i] < -h:
         print("CUSUM- an der Stelle  " + str(i) + "  zur Zeit  " + str(time[i]) + "  Minuten ab Start der Sonde.")
-        booln = False
+        s[i] = value_actual
+        sp[i] = value_actual
+        if sp[i] < 0:
+            sp[i] = 0  # sp wird nie < 0
+        sn[i] = value_actual
+        if sn[i] > 0:
+            sp[i] = 0  # sn wird nie > 0
+        detected_points.append(startdate_c + timedelta(minutes=time[i]))
+        i += 1000
     i += 1
 
-i -= 1
-if bool == True:
-    print("Kein CUSUM. s[" + str(i) + "] beträgt: " + str(s[i]))
-else:
-    print("s[" + str(i) + "] beträgt: " + str(s[i]))
-if boolp == True:
-    print("Kein CUSUM+. sp[" + str(i) + "] beträgt: " + str(s[i]))
-else:
-    print("sp[" + str(i) + "] beträgt: " + str(s[i]))
-if booln == True:
-    print("Kein CUSUM-. sn[" + str(i) + "] beträgt: " + str(s[i]))
-else:
-    print("sn[" + str(i) + "] beträgt: " + str(s[i]))
+for i in detected_points:
+    print(i)
+
 
 # Plotten der Ereignissanzahl (y-Achse) auf die Zeit (x-Achse):
 # TODO: Mit Marker und Zeitstempel
-detected_points[10000]=7718419
-detected_points[20000]=7730000
-detected_points[30000]=7740000
+#3 DP arrays are for testing
 plt.figure(1)
 plt.subplot(211)
-plt.plot(time, event_value, "g-")
+plt.plot(converted_time, event_value, "g-")
 plt.title('Event overview')
 plt.xlabel('Time after Start')
 plt.ylabel('#')
 for i in detected_points:
-	if i != 0:
-		plt.text(i, 40000, r'ED')
+        plt.text(i, 40000, r'ED')
+#Formating time axis
+plt.gcf().autofmt_xdate()
+myFmt = mdates.DateFormatter('%m-%d')
+plt.gca().xaxis.set_major_formatter(myFmt)
+
+
 
 plt.subplot(212)
 plt.plot(time_s, s, "b-",label='S')
@@ -100,6 +134,7 @@ plt.xlabel('Time after Start')
 plt.ylabel('#')
 legend = plt.legend(loc='upper left', shadow=True, fontsize='x-small')
 #legend.get_frame().set_facecolor("C0") Frabe der Legende
+
 
 
 ende = tm.time()
