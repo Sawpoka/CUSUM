bd7603c64374b6c27433bf90694de9438329ea02
Sawpoka <soeren.sander@gmx.de>
2019-01-29 16:52:51 +0100

    neueste Version inkl. dynamischen Mittelwert

--- cusum_v6.py
+++ cusum_v6.py
@@ -1,3 +1,5 @@
+# Vollversion CUSUM
+
 # benötigte Bibliotheken werden eingebunden
 
 import numpy as np
@@ -24,8 +26,14 @@ if number > len(time):
 
 start = tm.time() #Programmlaufzeit überprüfen
 
+#Variable Parameter:
+w = np.mean(event_value)	# Mittelwert setzen
+h = 100*w                   # Grenzwert
+std_abw_max = 150			# maximale Standartabweichung
+min_range = 100             # range for the new average value after an event
+i = 1;
 number = len(time) #Länge des zu überprüfenden Datensatzes
-w = np.mean(event_value) #Mittelwert (über alle Daten) setzen
+
 
 #Textausgabe im Terminal:
 print("Der eingelesene Datensatz enthaelt " + str(len(time)) + " Werte.")
@@ -41,7 +49,6 @@ cusum_p_points = []
 cusum_n_points = []
 converted_time = [0] * number #Array for saving of the converted times
 
-######TODO### Sinnvoll? - oder durchgängig bei Minuten bleiben, aber nur die Ausgaben umrechnen?
 
 #converting the time array into dates
 j = 0
@@ -49,77 +56,67 @@ while j < number:
     converted_time[j] = startdate_c + timedelta(minutes=time[j])
     j+=1
 
+#Falls gefordert wird hier die zu überprüfende Sequenz des Datensatzes erstellt
 time_s = time[:number]
 converted_time_s = converted_time[:number]
 
-h = 150*w                   # Grenzwert
-i = 1
+# Funktion: setzt neues w (Mittelwert dynamisch); neues i (Index wo das Event zuende ist) wird zurückgegeben
+def new_average_and_index(i):
+    std_abw = std_abw_max + 1
+    while (i+min_range) < number and std_abw > std_abw_max:
+        std_abw = np.std(event_value[i:(i+min_range)])
+        w = np.mean(event_value[i:(i+min_range)])
+        i += 100
+    return (i)
 
-############# event_over anpassen: neuer gemittelter Mittwert, sobald die Standartabweichung klein genug ist (Immer für 20-200 Werte ? <- variabel!)
-############# --> neues i (Position) und neues w (Mittelwert)
-
-#Funktion: definiert, wann Event zuende ist
-def event_over(i):
-    wp = 1.2 * w
-    wn = 0.8 * w
-    while i < number:
-        if event_value[i] < wp and event_value[i] > wn:
-            return i
-        else:
-            i += 1
 
 #Funktion: setzt neue Startwerte
-def new_start():
-        s[i] = value_actual
-        sp[i] = value_actual
+def new_start(i):
+        s[i] = value_current
+        sp[i] = value_current
         if sp[i] < 0:
             sp[i] = 0  # sp wird nie < 0
-        sn[i] = value_actual
+        sn[i] = value_current
         if sn[i] > 0:
             sp[i] = 0  # sn wird nie > 0
 
-# Startwerte setzen
-s[0] = event_value[0] - w
-sp[0] = event_value[0] - w
-if sp[0] < 0:
- sp[0] = 0  # sp wird nie < 0
-sn[0] = event_value[0] - w
-if sn[0] > 0:
- sp[0] = 0  # sn wird nie > 0
+# Startinitialisierungen:
+i = new_average_and_index(i)
+value_current = (event_value[i] - w)
+new_start(i)
 
 while i < number: #Hauptschleife
 
-    value_actual = (event_value[i] - w)
+    value_current = (event_value[i] - w)
 
-    s[i] = s[i-1] + value_actual ##########gemeint ist aktueller Wert (--> "current")
-    sp[i] = sp[i-1] + value_actual
+    s[i] = s[i-1] + value_current
+    sp[i] = sp[i-1] + value_current
     if sp[i] < 0:
         sp[i] = 0     # sp wird nie < 0
-    sn[i] = sn[i-1] + value_actual
+    sn[i] = sn[i-1] + value_current
     if sn[i] > 0:
         sn[i] = 0     # sn wird nie > 0
 
-    # Testet ob der Grenzwert überschritten wird und wenn ja, gibt dieses als Event aus und startet CUSUM neu, sobald das Event zuende ist
+    # Hier wird gestet ob der Grenzwert überschritten wird und wenn ja, wird dies als Event ausgegeben und startet CUSUM neu, sobald das Event zuende ist.
     if (s[i] < -h or s[i] > h):
         print("CUSUM an der Stelle  " + str(i) + "  zur Zeit  " + str(startdate_c + timedelta(minutes=time[i])) + ".")
         cusum_points.append(startdate_c + timedelta(minutes=time[i]))
-        new_start()
-        i = event_over(i)
+        new_start(i)
+        i = new_average_and_index(i)
     if sp[i] > h:
         print("CUSUM+ an der Stelle  " + str(i) + "  zur Zeit  " + str(startdate_c + timedelta(minutes=time[i])) + ".")
         cusum_p_points.append(startdate_c + timedelta(minutes=time[i]))
-        new_start()
-        i = event_over(i)
+        new_start(i)
+        i = new_average_and_index(i)
     if sn[i] < -h:
         print("CUSUM- an der Stelle  " + str(i) + "  zur Zeit  " + str(startdate_c + timedelta(minutes=time[i])) + ".")
         cusum_n_points.append(startdate_c + timedelta(minutes=time[i]))
-        new_start()
-        i = event_over(i)
+        new_start(i)
+        i = new_average_and_index(i)
 
     i += 1
 
 
-# print(detected_points)
 
 # Plotten der Ereignissanzahl (y-Achse) auf die Zeit (x-Achse):
 # TODO: Mit Marker und Zeitstempel
--- small_tests.py
+++ small_tests.py
@@ -2,5 +2,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.empty(20)
-print(a)
+
+time, event_value = np.loadtxt("Aufgabe7_CUSUM.txt", unpack = True)
+number = len(time)
+w = np.mean(event_value)
+std_abw_max = 100
+range = 100             # range for the new average value after an event
+
+
+def event_over(i):
+    wp = 1.2 * w
+    wn = 0.8 * w
+    while i < number:
+        if event_value[i] < wp and event_value[i] > wn:
+            return i
+        else:
+            i += 1
+
+i = 10000
+
+def new_average_and_index(i,range):
+    std_abw = std_abw_max + 1
+    while (i+range) < number and std_abw > std_abw_max:
+        std_abw = np.std(event_value[i:(i+range)])
+        w = np.mean(event_value[i:(i+range)])
+        i += 100
+    return std_abw, (i-99)
+
+
+std_abw, i = new_average_and_index(i,range)       # w geht offenbar so, i und std_abw muss zurückgegeben und neu überschrieben werden.
+print(i)
+print(std_abw)
+print(w)
